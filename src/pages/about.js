9e0216209c1a287ef5b715503756ce2c2522e2eb
Murilo Balducci Oliveira <murilo_oliveira@hst.br>
2020-07-06 23:31:43 -0300

    last update working

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const AboutPage = () => (
@@ -13,4 +11,4 @@ const AboutPage = () => (
   </> 
 )
 
-export default AboutPage;
+export default AboutPage
