0a6519b94dd49940e2139361c481b5c9dfb1da5c
Murilo Balducci Oliveira <murilo_oliveira@hst.br>
2020-07-07 10:38:35 -0300

    Sibebar part1 done - Profile/Avatar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,30 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Profile from "../Profile";
+import Sidebar from "../SideBar";
 
-import styled from "styled-components";
 import GlobalStyles from '../../styles/global';
+import * as S from './styled';
 
-const LayoutWrapper = styled.section`
-  display: flex;
-`
-
-const LayoutMain = styled.main`
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
-  width: 100%;
-`
 
 const Layout = ({ children }) => {
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyles />
-      <aside>
-        <Profile />
-      </aside>
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <Sidebar />
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
 
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,6 +3,8 @@ import { useStaticQuery, graphql } from 'gatsby';
 
 import Avatar from '../Avatar';
 
+import * as S from './styled';
+
 const Profile =() => {
   const {
     site: {
@@ -21,12 +23,17 @@ const Profile =() => {
   `);
 
   return(
-    <div className="Profile-wrapper">
+    <S.ProfileWrapper>
+      <S.ProfileLink>
       <Avatar />
-      <h1>{title}</h1>
-      <h2>{position}</h2>
-      <p>{description}</p>
-    </div>
+      <S.ProfileAuthor>
+        {title}
+        <S.ProfilePosition>{position}</S.ProfilePosition>
+        </S.ProfileAuthor>
+      
+      </S.ProfileLink>
+      <S.ProfileDescription>{description}</S.ProfileDescription>
+    </S.ProfileWrapper>
   )
   
 }
