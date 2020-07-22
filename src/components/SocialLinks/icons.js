b448757c05c1c9f29a3b36934362315e0febd58b
Murilo Balducci Oliveira <murilo_oliveira@hst.br>
2020-07-22 11:48:05 -0300

    change SocialLinks

--- src/components/SocialLinks/icons.js
+++ src/components/SocialLinks/icons.js
@@ -1,11 +1,11 @@
 import { Github } from '@styled-icons/boxicons-logos/Github';
 import { Instagram } from '@styled-icons/boxicons-logos/Instagram';
-import { Linkedin as LinkedIn } from '@styled-icons/boxicons-logos/Linkedin';
+import { Linkedin } from '@styled-icons/boxicons-logos/Linkedin';
 
 const Icons = {
   Github,
   Instagram,
-  LinkedIn
+  Linkedin
 }
 
 export default Icons;
