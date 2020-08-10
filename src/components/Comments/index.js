30e5934c96c26e7c622f66f92eddf15d0665649c
Murilo Balducci Oliveira <murilo_oliveira@hst.br>
2020-08-10 14:43:32 -0300

    comments update

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,13 +5,13 @@ import ReactDisqusComments from "react-disqus-comments"
 import * as S from "./styled"
 
 const Comments = ({ url, title }) => {
-  const completeURL = `murilobalducci.netlify.app${url}`
+  const completeURL = `https://murilobalducci.netlify.app/${url}`
 
   return (
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="Murilo Balducci"
+        shortname="murilobalducci"
         identifier={completeURL}
         title={title}
         url={completeURL}
