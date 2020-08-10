362726a47e1298df55d3db3d2dd51873afa8145b
Murilo Balducci Oliveira <murilo_oliveira@hst.br>
2020-08-10 14:51:12 -0300

    deploy comments

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -11,7 +11,7 @@ const Comments = ({ url, title }) => {
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="murilobalducci"
+        shortname="https-murilobalducci-netlify-app"
         identifier={completeURL}
         title={title}
         url={completeURL}
