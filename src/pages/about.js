74f94a4dc284c3b9e94060b62e50a9a13e4b9e54
Murilo Balducci <murilobalducci@gmail.com>
2020-08-10 22:16:52 -0300

    mudando pagina sobre mim

--- src/pages/about.js
+++ src/pages/about.js
@@ -40,21 +40,21 @@ const AboutPage = () => (
       </p>
 
       <p>
-        Sou noivo, muito feliz com minha família. Composta por minha mulher e nossa filha de quarto patas.
+        Sou noivo, muito feliz com minha família. Composta por minha mulher e nossa filha de quatro patas.
       </p>
 
       <p>
-        No meu tempo livre, costumo viajar, prático slack-line, gosto de correr, gosto de assitir um
-        Netflix, compartilho meu conhecimento através de blog e meu Instagram.
+        No meu tempo livre, costumo viajar, prático slack-line, gosto de correr, gosto de assitir
+        Netflix, compartilho meu conhecimento através do blog e meu Instagram.
       </p>
 
       <h2>Habilidades</h2>
 
       <p>
-        Meu início no mercado foi bem cedo aos meus 16 anos, sempre fui apaixonado por fazer dinheiro 
-        e ao mesmo tempo apaixonado por tecnologia. Antes de acabar o colégio já tinha comprado meu primeiro carro,
-        começei como vendedor e com muito estudo e dedicação em 6 meses de estudando Ciências Econômicas consegui um trabalho no <i>Banco Santander</i>,
-        durante meu período de muito aprendizado lá, tive a oportunidade e a vontade de morar na California, juntei minhas economias e fui.
+        Meu início no mercado foi bem cedo com 16 anos, sempre fui apaixonado por fazer dinheiro 
+        e ao mesmo tempo apaixonado por tecnologia. Antes de terminar o colégio já tinha comprado meu primeiro carro,
+        Logo após, entrei para faculdade, com muito estudo e dedicação em 6 meses no curso Ciências Econômicas consegui um trabalho no <i>Banco Santander</i>,
+        durante meu período de muito aprendizado no Santander, tive a oportunidade de morar na Calífornia, juntei tudo que tinha e fui.
         Aprendi muito e cresci muito, foi onde tive contato com o desenvolvimento na empresa <i>BitMonkeyDigital</i> (Marketing Digital, Suporte e Desenvolvimento Web).
         <br></br>
         Voltei para o Brasil, iniciei minha graduação em Sistemas de Informação, 
@@ -66,9 +66,7 @@ const AboutPage = () => (
       <p>
         Hoje estou focado no desenvolvimento Full Stack com
         Javascript, React, e SpringBoot e todo ecossistema por volta dessas
-        tecnologias. Também estou focado com o Marketing Digital onde 
-        fiz algumas mentorias, e não é conversinha fiada realmente dá pra ganhar dinheiro na internet, 
-        já faturei mais de 8 mil reais em poucos meses.
+        tecnologias.
       </p>
 
       <h2>Contato</h2>
